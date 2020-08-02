9a11b6442510796d112798c617cf2593512d17ad
小东 <49069586+x-dr@users.noreply.github.com>
2020-08-02 23:40:15 +0800

    xd

--- moe.py
+++ moe.py
@@ -3,7 +3,8 @@
 import requests, os, threading
 from lxml import etree
 
-Url = "https://www.moestack.com/all"
+#Url = "https://www.moestack.com/all"
+Url = "https://www.moestack.com/tunvlang"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"}
