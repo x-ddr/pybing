4a78d36c65ca6f2d8811c670ba49c938cbfb9da7
小东 <49069586+x-dr@users.noreply.github.com>
2020-08-03 05:21:28 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -4,7 +4,7 @@ import re
 
 
 
-for page in range (126,135):
+for page in range (134,135):
     print('-------------------正在爬取第{}页数据------------------'.format(page))
     # proxies = {'http': '60.190.23.50:8080'
     #            }
